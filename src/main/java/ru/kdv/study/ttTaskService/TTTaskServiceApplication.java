3c58d20b49746c91f3fa5e08bc81262068e5bffb
Kiryakov Denis <KiryakovDenis@gmail.com>
2025-04-01 10:37:29 +0500

    Initial Commit

---
+++ src/main/java/ru/kdv/study/ttTaskService/TTTaskServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.kdv.study.ttTaskService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TTTaskServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TTTaskServiceApplication.class, args);
+    }
+}
