57d9bdaaa2c51b5931a1bdd795050233956c94c7
Kiryakov Denis <KiryakovDenis@gmail.com>
2025-04-17 21:39:24 +0500

    Исправил заменчания по review, вынес url сервисов как внешние параметры

--- src/main/java/ru/kdv/study/ttTaskService/model/LogOperation.java
+++ src/main/java/ru/kdv/study/ttTaskService/model/LogOperation.java
@@ -1,16 +1,6 @@
 package ru.kdv.study.ttTaskService.model;
 
 public enum LogOperation {
-    I ("insert"),
-    U ("update");
-
-    private String desc;
-
-    LogOperation(String desc) {
-        this.desc = desc;
-    }
-
-    public String getDesc() {
-        return this.desc;
-    }
+    INSERT,
+    UPDATE;
 }
